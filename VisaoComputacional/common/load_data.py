acd7600c0e0e5c2541df6bb8f1d0a242126db5a9
derick-amontagna <derick.montagna@3778.care>
2022-11-27 18:22:20 -0300

    add a read imagens function

--- VisaoComputacional/common/load_data.py
+++ VisaoComputacional/common/load_data.py
@@ -4,23 +4,24 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import cv2
 from common.utils import logger
+import numpy as np
+from tqdm import tqdm
 
 
 # '..\\data\lgg-mri-segmentation\\kaggle_3m\\
 def load_data(path):
     image_mri = []
     image_masks = glob(path + "\\*\\*_mask*")
-
     logger.info(f"Loading MRIs and Masks".center(70, "+"))
     for i in image_masks:
         image_mri.append(i.replace("_mask", ""))
+        
     return image_mri, image_masks
 
 
 def split_data(image_mri, image_masks, test_size=0.1, val_size=0.15):
     """Return train, test, val"""
     mri = pd.DataFrame(data={"mri": image_mri, "mask": image_masks})
-
     logger.info(
         f"Split into: train ({(1-test_size-val_size)*100}%), test({(test_size)*100}%), val({(val_size)*100}%)".center(
             70, "+"
@@ -32,10 +33,43 @@ def split_data(image_mri, image_masks, test_size=0.1, val_size=0.15):
     return train, test, val
 
 
+def read_image(path):
+
+    x = cv2.imread(path, cv2.IMREAD_COLOR)
+    x = cv2.resize(x,(128,128))
+    x = x/255.0
+    x = x.astype(np.float32)
+
+    return x
+
+
+def read_mask(path):
+    x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    x = cv2.resize(x,(128,128))
+    x = x/255.0
+    x = x > 0.5
+    x = x.astype(np.float32)
+    x = np.expand_dims(x, axis=-1)
+
+    return x
+
+
+def read_images(paths, tipefile):
+    images = []
+    if tipefile == 'images':
+        for f in tqdm(paths):
+            img = read_image(f)
+            images.append(img)    
+    else:
+        for f in tqdm(paths):
+            img = read_mask(f)
+            images.append(img)
+    imgs1 = np.array(images)
+
+    return imgs1
+
 def show_mri_with_masks(image_mri, image_masks, rows: int = 3, columns: int = 3):
-
     fig = plt.figure(figsize=(10, 10))
-
     for i in range(1, rows * columns + 1):
         fig.add_subplot(rows, columns, i)
         img_path = image_mri[i]
@@ -46,5 +80,4 @@ def show_mri_with_masks(image_mri, image_masks, rows: int = 3, columns: int = 3)
         plt.imshow(img)
         plt.imshow(msk, alpha=0.4, cmap="gray")
         plt.axis("off")
-
     plt.show()
